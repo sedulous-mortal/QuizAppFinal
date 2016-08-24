5e87b74211bb75bb611aae93198ab85358a5694b
Alison Stuart <alisonnicolestuart@gmail.com>
2016-08-24 18:46:24 -0400

    Here are some functional fixes since my meeting with jpdevries.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 $('document').ready(function () {
     //counts how many questions have been answered
     var qCount = 0;
+    var points = 0;
 
     //here are all the questions
     var questions = [
@@ -22,15 +23,30 @@ $('document').ready(function () {
 
     },
         {
-            question: 'blah',
+            question: 'Assuming no ties, how many sets must a woman win in pro tennis to have won the match?',
 
-            answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+            answers: [' 2', ' 3', ' 4', ' 6'],
+            correctAns: 0,
+    },
+        {
+            question: 'What must one do to serve in the game of tennis?',
+
+            answers: ['You have to jump and then you can hit the ball.', 'You have to throw the ball up and then you can swing at it.', 'You first drop the ball, then hit it.', 'You must bounce the ball against the court, then swing at it.'],
             correctAns: 1,
-    }
-]
+
+},
+        {
+            question: 'What advertisement is Maria Sharapova famous for doing during her tennis career?',
+
+            answers: ['A sunglasses ad', 'A watch ad', 'A dress ad', 'A camera ad'],
+            correctAns: 3,
+
+    },
+];
 
 
     function generateQuestion() {
+        $('.qcount').text(qCount);
         $('.qText').text(questions[qCount].question);
         $('.answer[value=0]').text(questions[qCount].answers[0]);
         $('.answer[value=1]').text(questions[qCount].answers[1]);
@@ -39,39 +55,35 @@ $('document').ready(function () {
     }
 
     $('#startQuiz').click(function () {
+        $('#introDiv').hide();
+        $('#quizDiv').show();
         generateQuestion();
+
     });
 
     //submit handler
     $('#question').submit(function (e) {
         e.preventDefault();
+        console.log('Im about to run this conditional');
+        console.log($('input[name=option]:checked').attr('value'));
+        console.log(questions[qCount].correctAns);
+
         if ($('input[name=option]:checked').attr('value') == questions[qCount].correctAns) {
-            qCount = qCount + 1;
-            console.log("correct answer selected");
+            console.log('conditional evaluated true');
+            points++;
+            $('#points').text(points);
         }
+        qCount = qCount + 1;
+        $('.percentScore').text(points / qCount);
+        console.log(points + "is points");
+        console.log(qCount + "is question count");
+        generateQuestion();
 
+        if (qCount == 5) {
+            $('#resultsDiv').show();
+        }
     });
-    /*
-    //jquery handler for starting the quiz
-    startQuiz.click-- > takeQuiz()
 
-    takeQuiz: function () {
-            quizDiv.show();
-            for (i = 1, i <= 10, i++) {
-                this.id = ("q" + i);
-            }
 
-            nextQuestion: function () {
-                if ('this.answered == 0') {
-                    //SHOW NEXT QUESTION
-                }
-            }
 
-            showResults: function () {
-                document.write("Here are your results: you scored ____/10, or ____%, on this quiz.")
-            }
-
-            showResults();
-        } //close ready function
-        */
 }); //end ready function
